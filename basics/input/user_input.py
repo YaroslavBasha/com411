5bd25f0920cfe7ca1d8befd628ea499d6c6403e0
repl.it user <replituser@example.com>
2021-02-27 15:11:19 +0000

    Commit 1

---
+++ basics/input/user_input.py
@@ -0,0 +1,7 @@
+# Ask user to enter their name
+#print("What is your name human?")
+#name = input()
+
+# Read in user's name
+name = input("Please enter your name human: ")
+print("It is nice to meet you human", name)
